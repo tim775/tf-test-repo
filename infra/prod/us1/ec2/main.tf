b7a40f457b01fec496107d5be1bf234828646aec
Tim McFadden <52185+tim775@users.noreply.github.com>
2023-12-20 22:48:00 -0500

    Update main.tf

--- infra/prod/us1/ec2/main.tf
+++ infra/prod/us1/ec2/main.tf
@@ -9,4 +9,17 @@ provider "aws" {
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
   instance_type = "t3.xlarge"
+
+  tags = {
+    "bat"  = "ball"
+    "fizz" = "buzz"
+    "ef" = "rbd"
+  }
+
+  root_block_device {
+    volume_size = 50
+    tags = {
+      "ef" = "rbd"
+    }
+  }
 }
