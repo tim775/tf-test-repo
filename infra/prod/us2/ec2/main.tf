d9f8104b66ed078d81f68de1b2712a13f22f1fda
Tim McFadden <52185+tim775@users.noreply.github.com>
2023-12-21 21:46:32 -0500

    Update main.tf

--- infra/prod/us2/ec2/main.tf
+++ infra/prod/us2/ec2/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
-resource "aws_instance" "web_app" {
+resource "aws_instance" "web_apppp" {
   ami           = "ami-674cbc1e"
   instance_type = "t3.2xlarge"
 }
