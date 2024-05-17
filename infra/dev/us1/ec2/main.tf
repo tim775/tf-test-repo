a28f171b916e01c3fdb69b6a9ca43587a859c188
Tim McFadden <52185+tim775@users.noreply.github.com>
2024-05-17 13:22:08 -0400

    Update main.tf reduce 2x

--- infra/dev/us1/ec2/main.tf
+++ infra/dev/us1/ec2/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
-  instance_type = "t3.4xlarge"
+  instance_type = "t3.2xlarge"
 
   tags = {
     "bat"  = "ball"
