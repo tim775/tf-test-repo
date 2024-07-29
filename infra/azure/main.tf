082ce393c7e5bb9d4d94166562b1851f754e09c0
Tim McFadden <52185+tim775@users.noreply.github.com>
2024-07-29 09:10:30 -0400

    Add azure project

---
+++ infra/azure/main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+resource "azurerm_resource_group" "arg" {
+  name     = "arg"
+  location = "westus"
+}
+
+resource "azurerm_storage_account" "existing_passing" {
+  name                     = "existing_passing"
+  resource_group_name      = azurerm_resource_group.arg.name
+  location                 = azurerm_resource_group.arg.location
+  account_kind             = "BlobStorage"
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  blob_properties {
+    last_access_time_enabled = true
+  }
+}
+
+resource "azurerm_storage_management_policy" "existing_passing" {
+  storage_account_id = azurerm_storage_account.existing_passing.id
+}
+
+resource "azurerm_storage_account" "existing_failing" {
+  name                     = "existing_failing"
+  resource_group_name      = azurerm_resource_group.arg.name
+  location                 = azurerm_resource_group.arg.location
+  account_kind             = "BlobStorage"
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  blob_properties {
+  }
+}
