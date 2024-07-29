557c146b324076f7fac5167e34f85cf32dfd5525
Tim McFadden <52185+tim775@users.noreply.github.com>
2024-07-29 09:13:48 -0400

    Add azure "new_passing" and "new_failing"

--- infra/azure/main.tf
+++ infra/azure/main.tf
@@ -34,3 +34,31 @@ resource "azurerm_storage_account" "existing_failing" {
   blob_properties {
   }
 }
+
+resource "azurerm_storage_account" "new_passing" {
+  name                     = "new_passing"
+  resource_group_name      = azurerm_resource_group.arg.name
+  location                 = azurerm_resource_group.arg.location
+  account_kind             = "BlobStorage"
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  blob_properties {
+    last_access_time_enabled = true
+  }
+}
+
+resource "azurerm_storage_management_policy" "new_passing" {
+  storage_account_id = azurerm_storage_account.existing_passing.id
+}
+
+
+resource "azurerm_storage_account" "new_failing" {
+  name                     = "new_failing"
+  resource_group_name      = azurerm_resource_group.arg.name
+  location                 = azurerm_resource_group.arg.location
+  account_kind             = "BlobStorage"
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  blob_properties {
+  }
+}
